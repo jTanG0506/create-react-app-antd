4d726d78cec9ccf598c82c4277900f8f3fce8d0b
Jonathan Tang <hello@jtang.dev>
2020-05-11 19:53:48 +0100

    Change create-react-app files to TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Button } from 'antd';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Button type='primary' href='https://reactjs.org'>
+          Learn React
+        </Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;

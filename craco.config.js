ecf80f90213e2f6ddadc70a49478eb9b369a8bc0
Jonathan Tang <hello@jtang.dev>
2020-05-11 13:50:13 +0100

    Setup Craco to use CracoAntDesignPlugin

--- craco.config.js
+++ craco.config.js
@@ -1,1 +1,16 @@
-module.exports = {};
+const CracoAntDesignPlugin = require("craco-antd");
+const path = require("path");
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoAntDesignPlugin,
+      options: {
+        customizeThemeLessPath: path.join(
+          __dirname,
+          "src/styles/antOverrides.less"
+        ),
+      },
+    },
+  ],
+};
--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Button } from "antd";
 import logo from "./logo.svg";
+import "./styles/antOverrides.less";
 import "./App.css";
 
 function App() {
